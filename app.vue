0808942bd2798884749e4b07c87b700846e9c9d8
Andrew <aa4269205@gmail.com>
2025-06-25 17:27:36 +0800

    更新

--- app.vue
+++ app.vue
@@ -5,7 +5,11 @@ useHead({
   titleTemplate: '%s - 安卓部落格',
   meta: [
     { name: 'author', content: 'Andrew Chou' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      name: 'google-site-verification',
+      content: 'NRSu7JLVJg3AASfOMz_r7XU_d_3THDaTmfXxWMs4xiQ'
+    }
   ]
 })
 </script>
