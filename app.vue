813c6cc990e4975370e9a51e547f0e5700205fec
Andrew <aa4269205@gmail.com>
2025-06-26 11:24:36 +0800

    加入ga分析

--- app.vue
+++ app.vue
@@ -10,6 +10,20 @@ useHead({
       name: 'google-site-verification',
       content: 'NRSu7JLVJg3AASfOMz_r7XU_d_3THDaTmfXxWMs4xiQ'
     }
+  ],
+  script: [
+    {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-L875TBWW6W'
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-L875TBWW6W');
+      `
+    }
   ]
 })
 </script>
